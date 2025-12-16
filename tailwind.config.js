c49e30dcb960fbdc3f35b1756b89b023886bf656
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2025-12-16 15:32:26 +0530

    gitignore file is added, repository is reopened after a long time

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./RedeemCouponPage.html", "./RedeemCouponPage.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
